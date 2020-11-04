8a1f96388e149477c1a9167cd6996e1a657b711e
Ratnesh Karbhari <ratnesh@pop-os.localdomain>
2020-11-04 20:10:48 +0530

    made cart page responsive

--- ciFiles/app/Views/sitePages/cart.php
+++ ciFiles/app/Views/sitePages/cart.php
@@ -9,9 +9,9 @@
                             <th scope="col">Product Title</th>
                             <th scope="col">Material</th>
                             <th scope="col">Size</th>
-                            <th scope="col">Price</th>
+                            <th scope="col">Price (₹)</th>
                             <th scope="col">Quantity</th>
-                            <th scope="col">Total</th>
+                            <th scope="col">Total (₹)</th>
                             <th scope="col">Actions</th>
                         </tr>
                     </thead>
@@ -22,14 +22,14 @@
                             <td><?php echo $product['title']; ?></td>
                             <td><?php echo ucfirst($cart_item['material']); ?></td>
                             <td><?php echo ucfirst($cart_item['size']); ?></td>
-                            <td><?php echo '₹ '.$product['sale_price']; ?></td>
+                            <td><?php echo $product['sale_price']; ?></td>
                             <td>
                                 <form method="post" action="<?php echo site_url('update-cart'); ?>" style="display: inline;">
                                 <input type="hidden" name="cart-item-id" value='<?php echo $cart_item['id']; ?>'>
                                 <input type="number" name="product-qty" value="<?php echo $cart_item['quantity']; ?>" min="1" id="product-qty-<?php echo $cart_item['id']; ?>">
                             </td>
                             <td><?php if($cart_item['product_id']==$product['id']){
-                                echo '₹ '.$amount =$product['sale_price']*$cart_item['quantity'];
+                                echo $amount =$product['sale_price']*$cart_item['quantity'];
                                 $subtotal=$subtotal+$amount; 
                             } ?></td>
                             <td>
