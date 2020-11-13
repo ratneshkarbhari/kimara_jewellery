c4e29c05b69bc8fb26ccae647617b365d214e337
Ratnesh Karbhari <rkarbhari23@gmail.com>
2020-11-13 17:37:26 +0530

    saving collections

--- ciFiles/app/Views/sitePages/home.php
+++ ciFiles/app/Views/sitePages/home.php
@@ -165,7 +165,7 @@
                             <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" style="padding-top: 3%;">
                                 <div class="row" syyle="margin-top: 3%;">
 
-                                <?php $bsProducts = array_rand($products,5); foreach($bsProducts as $product): ?>
+                                <?php $bsProducts = array_rand($products,4); foreach($bsProducts as $product): ?>
 
                                     <div class="col-lg-3 col-md-6-sm-6 text-center custom-half-grid" style="margin-bottom: 2%; padding: 5px;"> <a href="<?php echo site_url('product/'.$product['slug']); ?>"><div class="card"> <img src="<?php echo site_url('assets/images/featured_image_product/'.$product['featured_image']); ?>" class="card-img-top"><div class="card-body">                            <h6 class="product-title"><?php if(strlen($product['title'])>9){
                                     echo substr($product['title'],0,9).'...';
@@ -198,7 +198,7 @@
                             
                                 <div class="row" syyle="margin-top: 3%;">
                                 
-                                <?php $topProds = array_rand($products,5); foreach($topProds as $product): ?>
+                                <?php $topProds = array_rand($products,4); foreach($topProds as $product): ?>
 
 
                                     <div class="col-lg-3 col-md-6-sm-6 text-center custom-half-grid" style="margin-bottom: 2%; padding: 5px;"> <a href="<?php echo site_url('product/'.$product['slug']); ?>"><div class="card"> <img src="<?php echo site_url('assets/images/featured_image_product/'.$product['featured_image']); ?>" class="card-img-top"><div class="card-body">                            <h6 class="product-title"><?php if(strlen($product['title'])>9){
