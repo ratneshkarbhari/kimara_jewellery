21f0b709b13130a6603b50bcfda62ee95e1e8b3f
Ratnesh Karbhari <rkarbhari23@gmail.com>
2020-12-15 10:32:05 +0530

    vendor login

--- ciFiles/app/Views/adminPages/dashboard.php
+++ ciFiles/app/Views/adminPages/dashboard.php
@@ -1,4 +1,4 @@
-<?php print_r($_SESSION); ?>
+
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4 page-content">
 
     <div class="container">
@@ -6,54 +6,42 @@
 
     <h3 class="page-title"><?php echo $title; ?></h3>
     
-        <?php if($_SESSION['approved']=='yes'): ?>
-
-            <div class="row text-center" style="margin: 3% 0;">
+        <div class="row text-center" style="margin: 3% 0;">
         
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                
-                    <a href="<?php echo site_url('manage-categories'); ?>">
-                        <div class="card custom-card-dashboard">
-                        
-                            Categories
-                        
-                        </div>
-                    </a>
-                
-                </div>
-                <div class="col-lg-6 col-md-6 col-sm-12">
-                    <a href="<?php echo site_url('manage-products'); ?>">
-                        <div class="card custom-card-dashboard">
-                        
-                            Products
-                        
-                        </div>
-                    </a>
-                
-                </div>
-                <div class="col-lg-12 col-md-12 col-sm-12">
-                
-                    <a href="<?php echo site_url('manage-orders'); ?>">
-                        <div class="card custom-card-dashboard">
-                        
-                            Orders
-                        
-                        </div>
-                    </a>
-                
-                </div>
+            <div class="col-lg-6 col-md-6 col-sm-12">
+            
+                <a href="<?php echo site_url('manage-categories'); ?>">
+                    <div class="card custom-card-dashboard">
+                    
+                        Categories
+                    
+                    </div>
+                </a>
             
             </div>
-
-        <?php elseif($_SESSION['approved']==''): ?>
-
-            <h4>Your Account is not yet approved please check.</h4>
-
-
+            <div class="col-lg-6 col-md-6 col-sm-12">
+                <a href="<?php echo site_url('manage-products'); ?>">
+                    <div class="card custom-card-dashboard">
+                    
+                        Products
+                    
+                    </div>
+                </a>
             
-
-
-        <?php endif; ?>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12">
+            
+                <a href="<?php echo site_url('manage-orders'); ?>">
+                    <div class="card custom-card-dashboard">
+                    
+                        Orders
+                    
+                    </div>
+                </a>
+            
+            </div>
+        
+        </div>
     
     </div>
 
--- ciFiles/app/Views/vendorPages/dashboard.php
+++ ciFiles/app/Views/vendorPages/dashboard.php
@@ -1,50 +1,60 @@
-
+<?php print_r($_SESSION); ?>
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4 page-content">
 
-<div class="container">
+    <div class="container">
 
 
     <h3 class="page-title"><?php echo $title; ?></h3>
-
-<?php print_r($_SESSION); ?>    
-
-    <div class="row text-center" style="margin: 3% 0;">
     
-        <div class="col-lg-6 col-md-6 col-sm-12">
+        <?php if($_SESSION['approved']=='yes'): ?>
+
+            <div class="row text-center" style="margin: 3% 0;">
         
-            <a href="<?php echo site_url('manage-categories'); ?>">
-                <div class="card custom-card-dashboard">
+                <div class="col-lg-6 col-md-6 col-sm-12">
                 
-                    Categories
+                    <a href="<?php echo site_url('manage-categories'); ?>">
+                        <div class="card custom-card-dashboard">
+                        
+                            Categories
+                        
+                        </div>
+                    </a>
                 
                 </div>
-            </a>
-        
-        </div>
-        <div class="col-lg-6 col-md-6 col-sm-12">
-            <a href="<?php echo site_url('manage-products'); ?>">
-                <div class="card custom-card-dashboard">
-                
-                    Products
-                
-                </div>
-            </a>
-        
-        </div>
-        <div class="col-lg-12 col-md-12 col-sm-12">
-        
-            <a href="<?php echo site_url('manage-orders'); ?>">
-                <div class="card custom-card-dashboard">
-                
-                    Orders
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <a href="<?php echo site_url('manage-products'); ?>">
+                        <div class="card custom-card-dashboard">
+                        
+                            Products
+                        
+                        </div>
+                    </a>
                 
                 </div>
-            </a>
-        
-        </div>
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                
+                    <a href="<?php echo site_url('manage-orders'); ?>">
+                        <div class="card custom-card-dashboard">
+                        
+                            Orders
+                        
+                        </div>
+                    </a>
+                
+                </div>
+            
+            </div>
+
+        <?php elseif($_SESSION['approved']=='no'): ?>
+
+            <h4>Your Account is not yet approved please check.</h4>
+
+
+            
+
+
+        <?php endif; ?>
     
     </div>
-
-</div>
 
 </main>
