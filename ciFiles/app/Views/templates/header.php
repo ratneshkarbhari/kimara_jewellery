064af315bed291c4ab1f33dde0a452ceedc825df
Ratnesh <ratneshkarbhari23@gmail.com>
2021-01-12 11:21:11 +0530

    added unveil

--- ciFiles/app/Views/templates/header.php
+++ ciFiles/app/Views/templates/header.php
@@ -13,7 +13,62 @@
 <script src="<?php echo site_url('assets/js/jquery.min.js'); ?>"></script>
 <script src="<?php echo site_url('assets/js/feather.min.js'); ?>"></script>
 <script>
-!function(t){t.fn.unveil=function(i,e){var n,r=t(window),o=i||0,u=window.devicePixelRatio>1?"data-src-retina":"data-src",s=this;function l(){var i=s.filter(function(){var i=t(this);if(!i.is(":hidden")){var e=r.scrollTop(),n=e+r.height(),u=i.offset().top;return u+i.height()>=e-o&&u<=n+o}});n=i.trigger("unveil"),s=s.not(n)}return this.one("unveil",function(){var t=this.getAttribute(u);(t=t||this.getAttribute("data-src"))&&(this.setAttribute("src",t),"function"==typeof e&&e.call(this))}),r.on("scroll.unveil resize.unveil lookup.unveil",l),l(),this}}(window.jQuery||window.Zepto);
+/**
+ * jQuery Unveil
+ * A very lightweight jQuery plugin to lazy load images
+ * http://luis-almeida.github.com/unveil
+ *
+ * Licensed under the MIT license.
+ * Copyright 2013 Luís Almeida
+ * https://github.com/luis-almeida
+ */
+
+;(function($) {
+
+$.fn.unveil = function(threshold, callback) {
+
+  var $w = $(window),
+      th = threshold || 0,
+      retina = window.devicePixelRatio > 1,
+      attrib = retina? "data-src-retina" : "data-src",
+      images = this,
+      loaded;
+
+  this.one("unveil", function() {
+    var source = this.getAttribute(attrib);
+    source = source || this.getAttribute("data-src");
+    if (source) {
+      this.setAttribute("src", source);
+      if (typeof callback === "function") callback.call(this);
+    }
+  });
+
+  function unveil() {
+    var inview = images.filter(function() {
+      var $e = $(this);
+      if ($e.is(":hidden")) return;
+
+      var wt = $w.scrollTop(),
+          wb = wt + $w.height(),
+          et = $e.offset().top,
+          eb = et + $e.height();
+
+      return eb >= wt - th && et <= wb + th;
+    });
+
+    loaded = inview.trigger("unveil");
+    images = images.not(loaded);
+  }
+
+  $w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
+
+  unveil();
+
+  return this;
+
+};
+
+})(window.jQuery || window.Zepto);
 </script>
     <header id="desktop" class="sticky-top">
         <!-- Global site tag (gtag.js) - Google Analytics -->
